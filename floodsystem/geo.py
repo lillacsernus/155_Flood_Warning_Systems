5f23fae11e63bc7819664ee431e36953cba69339
Lilla Csernus <lc868@cam.ac.uk>
2022-01-31 11:57:01 +0000

    added rivers_with_station() and stations_by_river() to geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -16,9 +16,9 @@ def stations_by_distance(stations, p):
     """This function returns a list of (station, distance) tuples 
     where distance is the distance from coordinate p"""
     distances = []
-    for n in range(len(stations)):
-        distance = haversine(stations[n].coord, p)
-        tuple = (stations[n].name, distance)
+    for n in stations:
+        distance = haversine(n.coord, p)
+        tuple = (n.name, distance)
         distances.append(tuple)
         distances_list = floodsystem.utils.sorted_by_key(distances, 1)
     return distances_list
@@ -27,9 +27,29 @@ def stations_within_radius(stations, centre, r):
     """This function returns a list of all stations (type MonitoringStation) 
     within radius r of a geographic coordinate x."""
     list_stations = []
-    for n in range(len(stations)):
-        radius = haversine(stations[n].coord, centre)
+    for n in stations:
+        radius = haversine(n.coord, centre)
         if radius <= r:
-            list_stations.append(stations[n].name)
-            list_stations.sort()
+            list_stations.append(n.name)
+    list_stations.sort()
     return list_stations
+
+def rivers_with_station(stations):
+    """This function, given a list of station objects, returns a container 
+    (list/tuple/set) with the names of the rivers with a monitoring station. """
+    x = set()
+    for n in stations:
+        x.add(n.river)
+    x_sorted = sorted(x)
+    return x_sorted
+
+def stations_by_river(stations):
+    """This function returns a Python dict (dictionary) that maps river 
+    names (the key) to a list of station objects on a given river."""
+    y = {}
+    for n in stations:
+        if n.river not in y:
+            y[n.river] = [n.name]
+        else: y[n.river].append(n.name)
+    return y
+    
